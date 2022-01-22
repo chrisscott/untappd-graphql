344a06a24a1f98e27bf301ca408bc0f74b5b9121
Chris Scott <chris@iamzed.com>
2022-01-22 10:44:18 -0500

    Update dependencies.

--- build/api/schema.js
+++ build/api/schema.js
@@ -7,13 +7,13 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _graphqlTools = require("graphql-tools");
+var _schema = require("@graphql-tools/schema");
 
 var _resolvers = _interopRequireDefault(require("./resolvers"));
 
 var _typeDefs = _interopRequireDefault(require("./typeDefs"));
 
-const schema = (0, _graphqlTools.makeExecutableSchema)({
+const schema = (0, _schema.makeExecutableSchema)({
   typeDefs: _typeDefs.default,
   resolvers: _resolvers.default
 });
